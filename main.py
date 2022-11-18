b973700d42b73de71771864d522c4b413755ed95
Allan Oliveira <87586984+allannoliveira@users.noreply.github.com>
2022-11-18 09:19:32 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,61 @@
 import random
+import PySimpleGUI as sg
+import os
+
+#layout
+sg.theme('Dark2')
+layout = [  [sg.Text('Menu')],
+            [sg.Text('(1) Senhas fortes')],
+            [sg.Text('(2) Senhas fracas ')],
+            [sg.Text('Digite sua escolha'), sg.InputText()],
+            [sg.Text(f'Sua nova senha é:  ')],
+            [sg.OK(), sg.Cancel()]]
+
+window = sg.Window('Window Title', layout)
+
+def senhaForte():
+    lower = "abcdefghijklmnopqrstuvwxyz"
+    upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    numbers = "0123456789"
+    symbols = "!@#$%¨&*(){}^~?/"
+
+
+    string = lower + upper + numbers + symbols
+    length = int(input("Qual o tamanho para sua senha: "))
+    password = "".join(random.sample(string, length))
+
+    print(f'Sua nova senha é: {password}')
+def senhaNumerica():
+    i = 0
+    password = []
+    
+    while i < 6:
+        
+        x = random.randint(1, 99)
+
+        password.append(x)
+
+        i += 1
+    print (f'Sua nova senha é: {password}')
+
+while True:
+    event, values = window.read()
+    if values == 1:
+        senhaForte()
+    elif values == 2:
+        senhaNumerica()
+
+    if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
+        break
+    print('You entered ', values[0])
+
+    window['-OUTPUT-'].update('Hello ' + values['-INPUT-'] + "! Thanks for trying PySimpleGUI")
+
+window.close()
+
+
+
+
 
 def senhaForte():
     lower = "abcdefghijklmnopqrstuvwxyz"
@@ -31,23 +88,10 @@ def senhaNumerica():
 
 
 
-
-
 def menu():
     print(''' Menu
     (1) - Senhas fortes
     (2) - Senha Numérica
     ''')
 
-menu()
 
-userinput = int(input('Digite o valor para criar sua senha nova: '))
-
-if userinput == 1:
-    senhaForte()
-    
-elif userinput == 2:
-    senhaNumerica()
-
-else:
-    print('Opção inválida ')
